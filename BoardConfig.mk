6fddea5711b470a58c6905adadc957a1be58cc2d
Dyneteve <sound0020@gmail.com>
2019-04-24 19:58:44 +0300

    violet: Update the memes
    * So we support br0lte
    * So its more meme
    * So its very much meme

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,16 +8,16 @@ DEVICE_PATH := device/xiaomi/violet
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75
+TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a75
+TARGET_2ND_CPU_VARIANT := cortex-a9
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
@@ -54,6 +54,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 # FM
 BOARD_HAS_QCA_FM_SOC := "cherokee"
 BOARD_HAVE_QCOM_FM := true
+
+# Treble
+BOARD_VNDK_VERSION := current
 
 # Partitions
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
--- device.mk
+++ device.mk
@@ -3,16 +3,21 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
+# Properties
+-include $(LOCAL_PATH)/system_prop.mk
+
 # Boot animation
-TARGET_SCREEN_HEIGHT := 4k
-TARGET_SCREEN_WIDTH := 4k
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
