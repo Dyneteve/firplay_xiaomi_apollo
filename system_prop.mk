9aa0b71c2bd9884193f79bf7b7e3075e9ceb4977
ItsVixano <vixanonew@gmail.com>
2021-03-16 10:48:38 +0100

    vaioeme: more fsp sar
    Change-Id: Ide02bd715cb4780095cd6c12e1618701ec2bc7a6

--- system_prop.mk
+++ system_prop.mk
@@ -15,3 +15,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.pubg.certified=true \
     ro.pubg.graphics.uhd=true \
     ro.pubg.in.unban=true
+
+# MORE FPS SARRRRRRRRRRRR
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.performace.mode=extreme \
+    ro.disable.thermals=true \
+    ro.disable.active.cooling=true \
+    ro.enable.battery.explosion=true \
+    ro.force.vsync.mode=847590843765978349fsp
