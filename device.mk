3e6e9c990abf4c7d1ebf989b258546f6d6121390
danascape <saalimquadri1@gmail.com>
2021-03-26 13:53:21 +0100

    apollo: Fix build * launched with S not Z oof

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_z.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_s.mk)
 
 # 5G
 PRODUCT_COPY_FILES += \
