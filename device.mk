85a5f57e78ef36db15a577c0f2d6aadc92baa51c
baalajimaestro <me@baalajimaestro.me>
2021-03-26 12:56:43 +0530

    apollo: Fix more build errors
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>

--- device.mk
+++ device.mk
@@ -6,7 +6,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_s_dp1.mk)
 
 # Properties
 -include $(LOCAL_PATH)/system_prop.mk
